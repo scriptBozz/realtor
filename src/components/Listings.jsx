7b45bbdff7932989fd06f712bdea12f792413711
Uche Nathans Wachuku <70748579+scriptBozz@users.noreply.github.com>
2023-05-24 08:27:44 +0000

    filter css

--- src/components/Listings.jsx
+++ src/components/Listings.jsx
@@ -16,6 +16,9 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import DomainIcon from '@mui/icons-material/Domain';
+import AddHomeIcon from '@mui/icons-material/AddHome';
+import AddHomeWorkSharpIcon from '@mui/icons-material/AddHomeWorkSharp';
 
 const ExpandMore = styled((props) => {
         const { expand, ...other } = props;
